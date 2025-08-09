1b5c39ede4d8f1a962973b309d74c22f8693a8b2
Eng-Bakar <abuukarmuuri2018@gmail.com>
2025-08-09 13:13:32 +0300

    Added update

--- Frontend/src/Components/Community_Comp/section1.jsx
+++ Frontend/src/Components/Community_Comp/section1.jsx
@@ -2,7 +2,12 @@ import Image1 from "../../Images/Community-Images/image.png"
 function SectionOne (){
     return <div className="mt-25 px-[100px]">
         <h1 className="text-[35px] font-bold ml-35 w-[600px]"> Acquiring and investing in startups and businesses. </h1>
-        <img className="w-[700px] rounded-xl" src={Image1} alt="" />
+        <div className="w-[700px] rounded-xl ml-35 mt-10 overflow-hidden ">
+            <img className=" w-full h-full object-cover transition-all duration-300 ease-in-out hover:scale-110" src={Image1} alt="" />
+        </div>
+        <h1 className="text-[35px] font-bold ml-35 w-[600px]"> At INNOVX, we acquire and invest in startups and businesses through our specialized investment vehicles, providing the support and resources they need to thrive. </h1>
+        <p> We invest directly in businesses across our three priority sectors, taking equity stakes so that we can maximise our impact and help businesses scale and grow. We also do greenfield investment, creating and developing new operations from the ground up.
+        We invest via our specialist funds such as BIDRA, UM6P and Seedbox and our objective is to develop high-performing businesses in agriculture, energy and chemicals that power sustainable growth and enable technology advancement. </p>
     </div>
 }
 export default SectionOne
