b01e558c8f69d3590eb4dda30c0d2292307ebfe3
Eng-Bakar <abuukarmuuri2018@gmail.com>
2025-08-21 10:56:39 +0300

    Update

--- Frontend/src/Pages/StartUps.jsx
+++ Frontend/src/Pages/StartUps.jsx
@@ -1,10 +1,12 @@
 import Footer from "../Components/Footer"
 import BottomPage from "../Components/Home_Comp/BottomPage"
 import PublicHeroSection from "../Components/PublicHeroSection"
+import Section1 from "../Components/StartUps_Comp/Section1"
 
 function Startups (){
     return <div>
         <PublicHeroSection />
+        <Section1 />
         <Footer />
         <BottomPage />
     </div>
