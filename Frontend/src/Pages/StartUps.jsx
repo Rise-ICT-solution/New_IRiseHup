96bfc0f975b1e14c1311efe9831c15eb8fe70ef8
AB- nazER <updynasir001@gmail.com>
2025-08-23 08:23:30 +0300

    jkl

--- Frontend/src/Pages/StartUps.jsx
+++ Frontend/src/Pages/StartUps.jsx
@@ -2,11 +2,17 @@ import Footer from "../Components/Footer"
 import BottomPage from "../Components/Home_Comp/BottomPage"
 import PublicHeroSection from "../Components/PublicHeroSection"
 import Section1 from "../Components/StartUps_Comp/Section1"
+import Section2 from "../Components/StartUps_Comp/Section2"
+import ValuesSection from "../Components/ValuesSection"
+import JoinUsSection from "../Components/JoinUsSection"
 
 function Startups (){
     return <div>
         <PublicHeroSection />
         <Section1 />
+        <Section2 />
+        <ValuesSection />
+        <JoinUsSection />
         <Footer />
         <BottomPage />
     </div>
